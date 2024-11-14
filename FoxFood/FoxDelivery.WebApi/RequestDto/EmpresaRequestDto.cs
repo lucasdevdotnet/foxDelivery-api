f05567511ce97d7ee5481fa62a4cb656c935892e
lucas <lucas.santana@pdcase.com.br>
2024-11-14 17:49:40 -0300

    ajustes

--- FoxFood/FoxDelivery.WebApi/RequestDto/EmpresaRequestDto.cs
+++ FoxFood/FoxDelivery.WebApi/RequestDto/EmpresaRequestDto.cs
@@ -4,8 +4,6 @@
     {
         public string Cnpj { get; set; }
         public string Motivo { get; set; }
-        public DateTime DataCadastro { get; set; }
-        public DateTime? DataAtualizacao { get; set; }
         public string NomeFantasia { get; set; }
         public string Nome { get; set; }
         public bool Ativo { get; set; }
